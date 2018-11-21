4075ecb1ae2c9756305a58c32cd084706ed38a35
harleenbajwa <39320022+harleenbajwa@users.noreply.github.com>
2018-11-20 19:26:47 -0700

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -3,7 +3,7 @@ Title: Precipitate Calculator
 Name: Harleen Bajwa
 Date: 11/07/18
 '''
-
+###NOTE: USE PbF2 as the compound
 ### variables ###
 
 posamount = 0
@@ -38,15 +38,15 @@ def ion():
 
 pos, neg = ion()
 compound = pos + neg
-print(compound)
+
 
 ## array 2d
 gr1 = [
-    ["LiF", 1, 1], ["MgF", 1, 2], ["CaF", 1, 2], ["SrF", 1, 2], ["BaF", 1, 2], ["FeF", 1, 2], ["HgF", ], ["PbF", 1, 2]
+    ["LiF", 1, 1], ["MgF", 1, 2], ["CaF", 1, 2], ["SrF", 1, 2], ["BaF", 1, 2], ["FeF", 1, 2], ["Hg2F", 1, 2 ], ["PbF", 1, 2]
 ]
 gr2 = [
-    ["CuCl", 1, 1], ["AgCl", 1, 1], ["HgCl", 1, 2], ["PbCl", 1, 2], ["TlCl", 1, 1], ["CuBr", 1, 1], ["AgBr", 1, 1],
-    ["HgBr", 1, 2], ["PbBr", 1, 2], ["TlBr", 1, 1], ["CuI", 1, 1], ["AgI", 1, 1], ["HgI", 1, 2], ["PbI", 1, 2], ["TlI", 1, 1]
+    ["CuCl", 1, 1], ["AgCl", 1, 1], ["Hg2Cl", 1, 2], ["PbCl", 1, 2], ["TlCl", 1, 1], ["CuBr", 1, 1], ["AgBr", 1, 1],
+    ["Hg2Br", 1, 2], ["PbBr", 1, 2], ["TlBr", 1, 1], ["CuI", 1, 1], ["AgI", 1, 1], ["Hg2I", 1, 2], ["PbI", 1, 2], ["TlI", 1, 1]
 ]
 gr3 = [
     ["CaSO4", 1, 1], ["SrSO4", 1, 1], ["BaSO4", 1, 1], ["AgSO4", 2, 1], ["Hg2SO4", 1, 1], ["PbSO4", 1, 1], ["RaSO4", 1, 1]
@@ -59,48 +59,41 @@ if pos in cation1:
   if neg == "F":
     gr = gr1
   else:
-    print("Elements are not recognized.")
+    print("Element(s) are not recognized.")
 elif pos in cation2:
   if neg == "Cl" or neg == "Br" or neg == "I":
     gr = gr2
   else:
-    print("Elements are not recognized.")
+    print("Element(s) are not recognized.")
 elif pos in cation3:
   if neg == "SO4":
     gr = gr3
   else:
-    print("Elements are not recognized.")
+    print("Element(s) are not recognized.")
 else:
-  print("Elements are not recognized.")
-balancing = []
-for i in range(len(gr)):
-    if compound == gr[i][0]:
-        balancing.append(gr[i][0].pop()) ## how would i do this
-        posamount = gr[i][1] + posamount
-        negamount = gr[i][2] + negamount
-      
+  print("Element(s) are not recognized.")
 
 
-"""
 try:
     for i in range(len(gr)):
-        if compound == gr[i][0]:
-            poscharge = gr[i][1] + poscharge
-            negcharge = gr[i][2] + negcharge
+      if compound == gr[i][0]:
+        posamount = gr[i][1] + posamount
+        negamount = gr[i][2] + negamount
 
     ## balancing the equation
-    if poscharge == negcharge:
+    if posamount == negamount:
         pass
-    elif (poscharge % negcharge == 0) or (negcharge % poscharge == 0):
-        if poscharge > negcharge:
-            prefix1 = str(int(poscharge / negcharge))
+    elif (posamount % negamount == 0) or (negamount % posamount == 0):
+        if posamount > negamount:
+            prefix1 = str(int(posamount / negamount))
         else:
-            prefix2 = str(int(negcharge / poscharge))
+            prefix2 = str(int(negamount / posamount))
     else:
-        prefix1 = str(negcharge)
-        prefix2 = str(poscharge)
-    print(prefix1 + pos + " + " + prefix2 + neg + " ---> " + compound)
-
+        prefix1 = negamount
+        prefix2 = posamount
+    pos = prefix1 + pos
+    neg = prefix2 + neg
+    compound = pos + prefix1 + neg + prefix2
+    print(compound)
 except NameError:
     print("This solution does not produce a precipitate.")
-"""
